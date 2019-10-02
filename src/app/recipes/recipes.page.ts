b3d7818e84060cba34487e9de5346f07a9578630
ShahrezaShahril <ibnuramadhan@yahoo.com>
2019-10-02 11:50:07 +0800

    update image and html

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-
+  recipes: Recipe[];
+  {
+    id: 'r1',
+    title: 'mee goreng',
+    imageUrl: 'https://i2.wp.com/angsarap.net/wp-content/uploads/2014/04/mee-goreng-wide.jpg',
+    ingredients: ['Mee Kuning', 'Sayur Sawi', 'Cili'],
+  },
+  {
+    id: 'r2',
+    title: 'nasi lemak',
+    imageUrl:'https://i.ytimg.com/vi/IUIs4C86Ilo/maxresdefault.jpg',
+    ingredients: ['Nasi', 'Santan'],
+  }
   constructor() { }
 
   ngOnInit() {
